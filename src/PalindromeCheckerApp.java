36b8c853d9e479071cb85323fd5de52ccc066604
ZIYA8909 <ziya3967@gmail.com>
2026-02-28 00:51:19 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,18 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
+        // UC1: Welcome message
         System.out.println("Welcome to Palindrome Checker App!");
+
+        // UC2: Hardcoded Palindrome Check
+        String word = "madam";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
     }
 }
